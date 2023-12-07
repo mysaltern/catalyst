c5c4c5a423ad5d19516a0e05921df861ec2488e7
mohammad <mysaltern@gmail.com>
2023-12-07 21:28:53 +1100

    Added User Upload Command Line Interface with File Parsing

--- app/Scripts/user_upload.php
+++ app/Scripts/user_upload.php
@@ -1,60 +1,72 @@
 <?php
 
-// Load Laravel's autoloader to access its functionalities
 require_once __DIR__ . '/../../vendor/autoload.php';
 
-// Boot up Laravel
 $app = require_once __DIR__ . '/../../bootstrap/app.php';
 $app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
 
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\DB;
-use App\Models\User; // Adjust the namespace according to your model location
+use App\Models\User;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
 
-// Read the CSV file
-$csvFile = fopen('./users.csv', 'r');
+$application = new Application('User Upload Script', '1.0.0');
 
-// Skip the header row
-$header = fgetcsv($csvFile);
+$application
+    ->register('user:upload')
+    ->setDescription('Uploads users from a CSV file to the database')
+    ->addOption('file', null, InputOption::VALUE_REQUIRED, 'Name of the CSV file to be parsed')
+    ->setCode(function (InputInterface $input, OutputInterface $output) {
+        $file = $input->getOption('file');
 
-// Begin a transaction
-DB::beginTransaction();
+        try {
+            $csvFile = @fopen($file, 'r');
+            if (!$csvFile) {
+                fwrite(STDOUT, "Error opening file $file");
+            }
+            // Begin a transaction
+            DB::beginTransaction();
 
-try {
-    while (($row = fgetcsv($csvFile)) !== false) {
-        $name = ucfirst(strtolower(trim($row[0])));
-        $surname = !empty($row[1]) ? ucfirst(strtolower(trim($row[1]))) : null;
-        $email = strtolower(trim($row[2]));
-        $validator = validator(['email' => $email], ['email' => 'required|email']);
+            while (($row = fgetcsv($csvFile)) !== false) {
+                $name = ucfirst(strtolower(trim($row[0])));
+                $surname = !empty($row[1]) ? ucfirst(strtolower(trim($row[1]))) : null;
+                $email = strtolower(trim($row[2]));
+                $validator = validator(['email' => $email], ['email' => 'required|email']);
 
-        // If email is invalid, skip insertion and log an error
-        if ($validator->fails()) {
-            fwrite(STDOUT, "Invalid email format for: $name $surname - $email\n");
-            continue;
+                if ($validator->fails()) {
+                    fwrite(STDOUT, "Invalid email format for: $name $surname - $email\n");
+                    continue;
+                }
+
+                $user = User::firstOrNew(['email' => $email]);
+
+                if (!$user->exists) {
+                    $user->fill([
+                        'name' => $name,
+                        'surname' => $surname,
+                    ])->save();
+                } else {
+                    fwrite(STDOUT, "User with email $email already exists. Skipped insertion.\n");
+                }
+            }
+
+            // Commit the transaction if all inserts are successful
+            DB::commit();
+            fclose($csvFile);
+        } catch (\Throwable $e) {
+            // Rollback the transaction on any error
+            DB::rollBack();
+
+            // Log the error message or handle the exception
+            Log::error('Error during CSV data insertion: ' . $e->getMessage());
+
+            if ($csvFile) {
+                fclose($csvFile);
+            }
         }
+    });
 
-        // Check if the user exists based on the email
-        $user = User::firstOrNew(['email' => $email]);
-
-        // Insert validated data into the database
-        // If the user doesn't exist, create a new user
-        if (!$user->exists) {
-            $user->fill([
-                'name' => $name,
-                'surname' => $surname,
-            ])->save();
-        } else {
-            fwrite(STDOUT, "User with email $email already exists. Skipped insertion.\n");        }
-    }
-    // Commit the transaction if all inserts are successful
-    DB::commit();
-} catch (\Throwable $e) {
-    // Rollback the transaction on any error
-    DB::rollBack();
-
-    // Log the error message or handle the exception
-    Log::error('Error during CSV data insertion: ' . $e->getMessage());
-} finally {
-    // Close the CSV file
-    fclose($csvFile);
-}
+$application->run();

0d233c7e271f3259c63a395443d8421d77313bb2
mohammad <mysaltern@gmail.com>
2023-12-12 12:52:58 +1100

    remove extra code

--- app/Resources/Console/UpdatedApplication.php
+++ app/Resources/Console/UpdatedApplication.php
@@ -39,7 +39,7 @@ class UpdatedApplication extends Application
         parent::__construct(static::NAME, static::VERSION);
 
         // manually add the commands you want to be handled by this
-        $this->add(new Mohammad());
+      //  $this->add(new Mohammad());
     }
 
 
@@ -144,14 +144,10 @@ class UpdatedApplication extends Application
     {
         return new InputDefinition([
             new InputArgument('command', InputArgument::REQUIRED, 'The command to execute'),
-            new InputOption('--help', '-H', InputOption::VALUE_NONE, 'Display help for the given command. When no command is given display help for the <info>'.$this->defaultCommand.'</info> command'),
-            new InputOption('--quiet', '-q', InputOption::VALUE_NONE, 'Do not output any message'),
-            new InputOption('--verbose', '-v|vv|vvv', InputOption::VALUE_NONE, 'Increase the verbosity of messages: 1 for normal output, 2 for more verbose output and 3 for debug'),
-            new InputOption('--version', '-V', InputOption::VALUE_NONE, 'Display this application version'),
-            new InputOption('--ansi', '', InputOption::VALUE_NEGATABLE, 'Force (or disable --no-ansi) ANSI output', null),
-            new InputOption('--no-interaction', '-n', InputOption::VALUE_NONE, 'Do not ask any interactive question'),
+//            new InputOption('--help', '-H', InputOption::VALUE_NONE, 'Display help for the given command. When no command is given display help for the <info>'.$this->defaultCommand.'</info> command'),
         ]);
 
     }
+   
 
 }
